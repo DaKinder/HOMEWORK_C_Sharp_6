ccca610c5d2d689c1195721185acd88903e95830
DaKinder <103947080+DaKinder@users.noreply.github.com>
2023-03-28 09:24:30 +0300

    initial commit

---
+++ 27032023/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW6
+{
+    public class MyClass
+    {
+        int _sum = 0;
+        readonly int _counter = 1;
+
+        int UserInput(int count)
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+
+        bool CheckNumber(int num)
+        {
+            if (num > 0) return true;
+            return false;
+        }
+
+        int CountNumbers(int num)
+        {
+            return _sum += num;
+        }
+
+        public int Processor(int count)
+        {
+            int result = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int num = UserInput(count);
+                bool flag = CheckNumber(num);
+                if (flag) result = CountNumbers(_counter);
+            }
+            return result;
+        }
+    }
+}
---
+++ 27032023/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Задача 41: Пользователь вводит с клавиатуры M чисел.
+            //Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+            //Стартовый блок
+            Console.WriteLine("Подсчитаем сколько чисел больше нуля");
+            Console.Write("Сколбко чисел будем считать? ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            //Создание экземпляра класса
+            MyClass counter= new MyClass();
+
+            //Блок вызова методов
+            int result = counter.Processor(count);
+
+            //Блок вывода в консоль
+            Console.WriteLine($"количество положительных чисел = {result}");
+            Console.ReadLine();
+
+
+            //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+            //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+            //значения b1, k1, b2 и k2 задаются пользователем.
+
+
+        }
+    }
+}
